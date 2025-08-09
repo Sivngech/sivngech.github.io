4635d2154bc135e298b6541f4127dfb3d9b3ea97
Your NameSivngech <you@example.comsivngech2@gmail.com>
2025-08-10 01:28:03 +0700

    update

--- components.js
+++ components.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <nav class="d-flex justify-content-center justify-content-md-end gap-3">
             <a href="index.html" class="text-decoration-none text-dark fw-semibold">Home</a>
             <a href="about-me.html" class="text-decoration-none text-dark fw-semibold">About Me</a>
-            <a href="#" class="text-decoration-none text-dark fw-semibold">Contact</a>
+            <a href="#contact" class="text-decoration-none text-dark fw-semibold">Contact</a>
           </nav>
         </div>
       </div>
@@ -37,17 +37,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
       <!-- Middle: Social Icons -->
       <div class="d-flex gap-3 text-white">
-        <a href="https://facebook.com" target="_blank" class="text-white" aria-label="Facebook">
+        <a href="https://www.facebook.com/sivngech.khim?mibextid=wwXIfr&mibextid=wwXIfr" target="_blank" class="text-white" aria-label="Facebook">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor">
             <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
           </svg>
         </a>
-        <a href="#" target="_blank" class="text-white" aria-label="Instagram">
+        <a href="https://t.me/call/I-jngMD5jlYC1zMHSP7IA3-9OUg" target="_blank" class="text-white" aria-label="Instagram">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
   <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
 </svg>
         </a>
-        <a href="https://t.me" target="_blank" class="text-white" aria-label="Telegram">
+        <a href="https://t.me/Sivngech_khim" target="_blank" class="text-white" aria-label="Telegram">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
             viewBox="0 0 16 16">
             <path
